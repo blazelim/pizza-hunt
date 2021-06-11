26d1510a05803b8c9bb31768118f9abd4ddbede8
Blaze Lim <blazebentleycolim@gmail.com>
2021-06-10 17:20:08 -0700

    added comment mode

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,42 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
-    },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Comment',
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }],
+    },    
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true
+            },
+            id: false
+        }
+);
+
+PizzaSchema.virtual('commentCount').get(function() {
+
+        return this.comments.length;
+    
 });
 
 const Pizza = model ('Pizza', PizzaSchema)
